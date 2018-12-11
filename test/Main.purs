77d35ee1d3be690b696ee74952f1e98b37753422
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-12-11 11:19:02 +0100

    update examples

--- test/Main.purs
+++ test/Main.purs
@@ -1,43 +1,98 @@
-module Main where
+module Main
+  ( module Data.Generic.Rep
+  , module Type.Reflection
+  , None
+  , Only, o
+  , Pair, p
+  , More, m
+  , List, l
+  , Tree, t
+  ) where
 
 
-import Data.Generic.Rep
+import Data.Generic.Rep (class Generic, from, to)
+
 import Type.Reflection
 
 
 
+-- None ------------------------------------------------------------------------
+
+
 data None
+
+derive instance genericNone :: Generic None _
+
+
+
+-- Only ------------------------------------------------------------------------
 
 
 data Only
   = Only
 
+derive instance genericOnly :: Generic Only _
+
+
+o :: Only
+o = Only
+
+
+
+-- Pair ------------------------------------------------------------------------
+
 
 data Pair a b
   = Pair a b
+
+derive instance genericPair :: Generic (Pair a b) _
+
+
+p :: Pair Int Int
+p = Pair 1 2
+
+
+
+-- More ------------------------------------------------------------------------
 
 
 data More a b
   = Zero
   | One a
   | Two a b
-  | Three (Array (Array (Pair a b)))
+
+derive instance genericTest :: Generic (More a b) _
+
+
+m :: More Int String
+m = Two 1 "a"
+
+
+
+-- List ------------------------------------------------------------------------
 
 
 data List a
   = Nil
   | Cons a (List a)
 
+derive instance genericList :: Generic (List a) _
+
+
+l :: List Int
+l = Cons 1 (Cons 0 Nil)
+
+
+
+-- Tree ------------------------------------------------------------------------
+
 
 data Tree a b
   = Leaf a
   | Node (Tree a b) b (Tree a b)
 
-
-
-derive instance genericVoid :: Generic None _
-derive instance genericUnit :: Generic Only _
-derive instance genericTest :: Generic (More a b) _
-derive instance genericList :: Generic (List a) _
-derive instance genericPair :: Generic (Pair a b) _
 derive instance genericTree :: Generic (Tree a b) _
+
+
+t :: Tree Int String
+t = Node (Leaf 1) "a" (Leaf 2)
