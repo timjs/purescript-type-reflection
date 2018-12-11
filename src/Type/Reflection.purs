52792dc0b5287a0020864bc4010ad11cd0be589c
Tim Steenvoorden <tim.steenvoorden@gmail.com>
2018-12-11 11:09:38 +0100

    merge Typeable and GenericTypeable

--- src/Type/Reflection.purs
+++ src/Type/Reflection.purs
@@ -3,11 +3,19 @@ module Type.Reflection where
 
 import Prelude
 
+-- import Prim.TypeError (class Fail, Beside, Quote, Text)
+
 import Data.Generic.Rep (class Generic, Argument, Constructor, NoArguments, NoConstructors, Product, Sum)
 import Data.Symbol (class IsSymbol, SProxy(..), reflectSymbol)
 
 import Unsafe.Coerce (unsafeCoerce)
 
+
+quote :: String -> String
+quote s = "\"" <> s <> "\""
+
+group :: String -> String
+group s = "(" <> s <> ")"
 
 
 -- Type representations --------------------------------------------------------
@@ -20,106 +28,110 @@ data TypeRep r
   | NoConstructors
   | Product (forall p. (forall a b. TypeRep a -> TypeRep b -> p) -> p)
   | Sum (forall p. (forall a b. TypeRep a -> TypeRep b -> p) -> p)
+  | Boolean
   | Int
   | Number
+  | Char
   | String
   | Array (forall p. (forall a. TypeRep a -> p) -> p)
+  | Function (forall p. (forall a b. TypeRep a -> TypeRep b -> p) -> p)
 
 
 instance showTypeRep :: Show (TypeRep r) where
   show (Argument unpack) = unpack \inner ->
-    "(Argument " <> show inner <> ")"
-  show NoArguments = "NoArguments"
-  show (Constructor unpack) = unpack \name inner ->
-    "(Constructor \"" <> name <> "\" " <> show inner <> ")"
-  show NoConstructors = "NoConstructors"
+    -- "(Argument " <> show inner <> ")"
+    group $ show inner
+  show NoArguments =
+    -- "NoArguments"
+    "_"
+  show (Constructor unpack) = unpack \name prod ->
+    -- "(Constructor \"" <> name <> "\" " <> show prod <> ")"
+    name <> " " <> show prod
+  show NoConstructors = "_"
   show (Product unpack) = unpack \left right ->
-    "(Product " <> show left <> " " <> show right <> ")"
+    -- "(Product " <> show left <> " " <> show right <> ")"
+    show left <> " " <> show right
   show (Sum unpack) = unpack \left right ->
-    "(Sum " <> show left <> " " <> show right <> ")"
+    -- "(Sum " <> show left <> " " <> show right <> ")"
+    show left <> " | " <> show right
+  show Boolean = "Boolean"
   show Int = "Int"
   show Number = "Number"
+  show Char = "Char"
   show String = "String"
   show (Array unpack) = unpack \inner ->
-    "(Array " <> show inner <> ")"
-
-
-class GenericTypeable r where
-  typeRep' :: TypeRep r
-
-
-instance genericTypeableArgument ::
-  Typeable a => GenericTypeable (Argument a)
-  where
-    typeRep' = Argument \pack -> pack (typeRep :: TypeRep a)
-
-
-instance genericTypeableNoArguments ::
-  GenericTypeable NoArguments where
-    typeRep' = NoArguments
-
-
-instance genericTypeableConstructor ::
-  (IsSymbol name, GenericTypeable a) => GenericTypeable (Constructor name a)
-  where
-    typeRep' = Constructor \pack -> pack ctor rep
-      where
-        ctor = reflectSymbol (SProxy :: SProxy name)
-        rep = typeRep' :: TypeRep a
-
-
-instance genericTypeableNoConstructors ::
-  GenericTypeable NoConstructors
-  where
-    typeRep' = NoConstructors
-
-
-instance genericTypeableSum ::
-  (GenericTypeable a, GenericTypeable b) => GenericTypeable (Sum a b)
-  where
-    typeRep' = Sum \pack ->
-      pack (typeRep' :: TypeRep a) (typeRep' :: TypeRep b)
-
-
-instance genericTypeableProduct ::
-  (GenericTypeable a, GenericTypeable b) => GenericTypeable (Product a b)
-  where
-    typeRep' = Product \pack ->
-      pack (typeRep' :: TypeRep a) (typeRep' :: TypeRep b)
-
+    -- "(Array " <> show inner <> ")"
+    "Array " <> group (show inner)
+  show (Function unpack) = unpack \from to ->
+    -- "(" <> show from <> " -> " <> show to <> ")"
+    show from <> " -> " <> show to
 
 
 -- Typeable --------------------------------------------------------------------
 
 
--- type SomeTypeRep = Pack (forall r. TypeRep r)
-
-
 class Typeable a where
   typeRep :: TypeRep a
 
-instance typeableInt ::
+
+instance typeableBoolean ::
+  Typeable Boolean where
+    typeRep = Boolean
+else instance typeableInt ::
   Typeable Int where
     typeRep = Int
 else instance typeableNumber ::
   Typeable Number where
     typeRep = Number
+else instance typeableChar ::
+  Typeable Char where
+    typeRep = Char
 else instance typeableString ::
   Typeable String where
     typeRep = String
+
 else instance typeableArray ::
   Typeable a => Typeable (Array a) where
     typeRep = Array \pack -> pack (typeRep :: TypeRep a)
+else instance typeableFunction ::
+  (Typeable a, Typeable b) => Typeable (Function a b) where
+    typeRep = Function \pack -> pack (typeRep :: TypeRep a) (typeRep :: TypeRep b)
+
+else instance typeableArgument ::
+  Typeable a => Typeable (Argument a)
+  where
+    typeRep = Argument \pack -> pack (typeRep :: TypeRep a)
+else instance typeableNoArguments ::
+  Typeable NoArguments where
+    typeRep = NoArguments
+else instance typeableConstructor ::
+  (IsSymbol name, Typeable a) => Typeable (Constructor name a)
+  where
+    typeRep = Constructor \pack ->
+      pack (reflectSymbol (SProxy :: SProxy name)) (typeRep :: TypeRep a)
+else instance typeableNoConstructors ::
+  Typeable NoConstructors
+  where
+    typeRep = NoConstructors
+else instance typeableSum ::
+  (Typeable a, Typeable b) => Typeable (Sum a b)
+  where
+    typeRep = Sum \pack ->
+      pack (typeRep :: TypeRep a) (typeRep :: TypeRep b)
+else instance typeableProduct ::
+  (Typeable a, Typeable b) => Typeable (Product a b)
+  where
+    typeRep = Product \pack ->
+      pack (typeRep :: TypeRep a) (typeRep :: TypeRep b)
+
 else instance typeableGeneric ::
-  (Generic a r, GenericTypeable r) => Typeable a where
-    typeRep = unsafeCoerce (typeRep' :: TypeRep r)
+  (Generic a r, Typeable r) => Typeable a where
+    typeRep = unsafeCoerce (typeRep :: TypeRep r)
+
+-- else instance typeableFail ::
+--   Fail (Beside (Text "Can not generate a type representation for ") (Quote a)) => Typeable a where
+--     typeRep = unsafeCoerce unit
 
 
 typeOf :: forall a. Typeable a => a -> TypeRep a
 typeOf _ = typeRep
-
-
--- typeRep :: forall a r.
---   Generic a r => GenericTypeable r =>
---   a -> TypeRep a
--- typeRep _ = unsafeCoerce (typeRep' :: TypeRep r)
--- test/Main.purs
+++ test/Main.purs
@@ -5,6 +5,7 @@ import Data.Generic.Rep
 
 
 
+
 data None
 
 
@@ -12,12 +13,24 @@ data Only
   = Only
 
 
+data Pair a b
+  = Pair a b
+
+
 data More a b
   = Zero
   | One a
   | Two a b
+  | Three (Array (Array (Pair a b)))
+
+
+data List a
+  = Nil
+  | Cons a (List a)
 
 
 derive instance genericVoid :: Generic None _
 derive instance genericUnit :: Generic Only _
 derive instance genericTest :: Generic (More a b) _
+derive instance genericList :: Generic (List a) _
+derive instance genericPair :: Generic (Pair a b) _
